af6299c588e7983a0ddb2ac82ec3695e7cfb3c56
azatkariuly <azat.kariuly@gmail.com>
2021-11-18 16:33:58 +0900

    initial commit

--- models/binarized_modules.py
+++ models/binarized_modules.py
@@ -89,24 +89,16 @@ class BinarizeConv2d(nn.Conv2d):
         return out
 
 def OA(x, b=4):
-    mask = (1 << b) - 1
-    mask2 = 2**(b-1)
+    out = x%2**b
+    mask = 2**(b-1)
 
     Qn = -2**(b-1)
     Qp = 2**(b-1)-1
 
-    upper = (x > Qp).float()
-    lower = (x < Qn).float()
+    upper = (out > Qp).float()
+    lower = (out < Qn).float()
     middle = 1.0 - upper - lower
 
-    out = x*middle
+    out = out*middle + (out-2*mask)*upper + (out+2*mask)*lower
 
-    out2 = (x*(upper+lower)).int()&mask
-
-    upper2 = (out2 > Qp).float()
-    lower2 = (out2 < Qn).float()
-    middle2 = 1.0 - upper2 - lower2
-
-    out3 = out2*middle2 + (out2-2*mask2)*upper2 + (out2+2*mask2)*lower2
-
-    return out+out3
+    return out
