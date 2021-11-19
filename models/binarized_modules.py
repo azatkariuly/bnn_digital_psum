e53babe662cfd7f03bcc27cd26331ac294b0b8c4
azatkariuly <azat.kariuly@gmail.com>
2021-11-19 13:55:04 +0900

    initial commit

--- models/binarized_modules.py
+++ models/binarized_modules.py
@@ -33,9 +33,9 @@ def satmm_cuda_temp(A, X, T=64, b=8, signed=True, nbits_psum=8, step_size_psum=N
 
     #print(psum.max(), psum.min())
     if step_size_psum is not None:
-        print('Quantization')
         psum, s = quantizeLSQ_psum(psum, step_size_psum, nbits_psum)
-        return OA(torch.sum(psum, axis=-1), b=b)*s
+        print('OA', b, ' nbits_psum', nbits_psum)
+        return OA(torch.sum(psum, axis=3).squeeze().transpose(1,-1), b=b)*s
 
     out = torch.sum(psum, axis=3).squeeze().transpose(1,-1)
     return OA(out, b=b)
