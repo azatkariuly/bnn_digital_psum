9fba1d4c1ec2bb49fde34a25dbab4afe9c302a40
azatkariuly <azat.kariuly@gmail.com>
2021-11-21 14:55:26 +0900

    initial commit

--- cursor.py
+++ cursor.py
@@ -4,5 +4,4 @@ sys.path.append(os.path.join(os.path.dirname(__file__), 'progress'))
 from progress.bar import Bar as Bar
 
 bar = Bar('Processing', max=1)
-bar.suffix  = '{phase}'.format(phase='Cursome, come back!')
 bar.finish()
