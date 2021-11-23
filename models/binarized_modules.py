97909c90c0a65ab8cb6bc015024bf673e2f4badd
azatkariuly <azat.kariuly@gmail.com>
2021-11-23 17:28:45 +0900

    initial commit

--- models/binarized_modules.py
+++ models/binarized_modules.py
@@ -38,8 +38,8 @@ def satmm_cuda_temp(A, X, T=64, b=8, signed=True, nbits_psum=8, step_size_psum=N
     if step_size_psum is not None:
         psum, s = quantizeLSQ_psum(psum, step_size_psum, nbits_psum)
         #out = reduce(lambda x,y: (x+y).clip(min, max), psum.transpose(0,3)).squeeze().transpose(0,-1)*step_size_psum
-        #out = OA(torch.sum(psum, axis=3).squeeze().transpose(1,-1), b=b)*step_size_psum
-        out = cyclic_activation(torch.sum(psum, axis=3).squeeze().transpose(1,-1), k=2, b=b)*step_size_psum
+        out = OA(torch.sum(psum, axis=3).squeeze().transpose(1,-1), b=b)*step_size_psum
+        #out = cyclic_activation(torch.sum(psum, axis=3).squeeze().transpose(1,-1), k=2, b=b)*step_size_psum
         return out
 
     out = reduce(lambda x,y: (x+y).clip(min, max), psum.transpose(0,3)).squeeze().transpose(0,-1)
@@ -191,30 +191,9 @@ class BinarizeConv2d(nn.Conv2d):
 
 def OA(x, b=4):
     return (x+2**(b-1)).remainder(2**b) - 2**(b-1)
-    mask = (1 << b) - 1
-    mask2 = 2**(b-1)
-
-    Qn = -2**(b-1)
-    Qp = 2**(b-1)-1
-
-    upper = (x > Qp).float()
-    lower = (x < Qn).float()
-    middle = 1.0 - upper - lower
-
-    out = x*middle
-
-    out2 = (x*(upper+lower)).int()&mask
-
-    upper2 = (out2 > Qp).float()
-    lower2 = (out2 < Qn).float()
-    middle2 = 1.0 - upper2 - lower2
-
-    out3 = out2*middle2 + (out2-2*mask2)*upper2 + (out2+2*mask2)*lower2
-
-    return out+out3
 
 def cyclic_activation(z, k, b):
-    m = (z+2**(b-1)).remainder(2**b) - 2**(b-1) #OA
+    #m = (z+2**(b-1)).remainder(2**b) - 2**(b-1) #OA
 
     Q = k*(2**(b-1))/(k+1)
 
