2e631235df2c578dcf3bb43eaf6dbab8ffd29380
azatkariuly <azat.kariuly@gmail.com>
2021-11-24 18:01:55 +0900

    initial commit

--- models/binarized_modules_wrapnet.py
+++ models/binarized_modules_wrapnet.py
@@ -122,7 +122,7 @@ class BinarizeConv2d(nn.Conv2d):
         #                T=self.T, b=self.nbits_acc, signed=True,
         #                nbits_psum=self.nbits_acc, step_size_psum=self.step_size_psum)
 
-        out = OA(out, b=self.nbits_acc).float() + out - out.int()
+
 
         if not self.bias is None:
             print("WHAT THE FUCK")
@@ -131,6 +131,7 @@ class BinarizeConv2d(nn.Conv2d):
 
         r = regularizer(out, b=self.nbits_acc)
         #WrapNet cyclic activation
+        out = OA(out, b=self.nbits_acc).float() + out - out.int()
         #out = cyclic_activation(out, k=self.k, b=self.nbits_acc)
 
         return out, 0
