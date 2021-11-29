fa39d28a5f0fe01158571772f57d60f06e358bdb
azatkariuly <azat.kariuly@gmail.com>
2021-11-29 11:56:59 +0900

    initial commit

--- imagenet/birealnet.py
+++ imagenet/birealnet.py
@@ -126,14 +126,14 @@ class HardBinaryConv(nn.Module):
         scaling_factor = torch.mean(torch.mean(torch.mean(abs(real_weights),dim=3,keepdim=True),dim=2,keepdim=True),dim=1,keepdim=True)
         #print(scaling_factor, flush=True)
         scaling_factor = scaling_factor.detach()
-        binary_weights_no_grad = scaling_factor * torch.sign(real_weights)
+        binary_weights_no_grad = torch.sign(real_weights)
         cliped_weights = torch.clamp(real_weights, -1.0, 1.0)
         binary_weights = binary_weights_no_grad.detach() - cliped_weights.detach() + cliped_weights
-        print(binary_weights, flush=True)
-        y = F.conv2d(x, binary_weights, stride=self.stride, padding=self.padding)
+        #print(binary_weights, flush=True)
+        y = F.conv2d(x, binary_weights, stride=self.stride, padding=self.padding) * scaling_factor
 
         #print(y.max(), y.min())
-        y = OA(y.int(), b=self.nbits_acc).float() + y - y.int()
+        #y = OA(y.int(), b=self.nbits_acc).float() + y - y.int()
 
         return y
 
