3f6426a1bbd70c5fbb174ee785920266b7d9c231
azatkariuly <azat.kariuly@gmail.com>
2021-11-29 10:15:59 +0900

    initial commit

--- imagenet/main.py
+++ imagenet/main.py
@@ -30,7 +30,7 @@ parser.add_argument('--epochs', type=int, default=256, help='num of training epo
 parser.add_argument('--learning_rate', type=float, default=0.001, help='init learning rate')
 parser.add_argument('--momentum', type=float, default=0.9, help='momentum')
 parser.add_argument('--weight_decay', type=float, default=0, help='weight decay')
-parser.add_argument('--save', type=str, default='./models', help='path for saving trained models')
+parser.add_argument('--save', type=str, default='./results', help='path for saving trained models')
 parser.add_argument('--data', metavar='DIR', default='/Dataset/ILSVRC2012/', help='path to dataset')
 parser.add_argument('--label_smooth', type=float, default=0.1, help='label smoothing')
 parser.add_argument('-j', '--workers', default=40, type=int, metavar='N',
@@ -61,6 +61,7 @@ def main():
     # load model
     model = birealnet18()
     logging.info(model)
+    return
     model = nn.DataParallel(model).cuda()
 
     criterion = nn.CrossEntropyLoss()
