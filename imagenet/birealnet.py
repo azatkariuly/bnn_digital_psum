d4086fd001f6c73f452a1affbecae3005cf7f244
azatkariuly <azat.kariuly@gmail.com>
2021-11-29 12:20:26 +0900

    initial commit

--- imagenet/birealnet.py
+++ imagenet/birealnet.py
@@ -131,8 +131,8 @@ class HardBinaryConv(nn.Module):
         binary_weights = binary_weights_no_grad.detach() - cliped_weights.detach() + cliped_weights
         #print(binary_weights, flush=True)
         y = F.conv2d(x, binary_weights, stride=self.stride, padding=self.padding)
-        print(y.max(), y.min())
-        #y = OA(y.int(), b=self.nbits_acc).float() + y - y.int()
+
+        y = OA(y.int(), b=self.nbits_acc).float() + y - y.int()
 
         return y*scaling_factor.reshape(1, -1, 1, 1)
 
