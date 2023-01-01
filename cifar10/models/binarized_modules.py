b04327278e7a96635c3b2e9bfbd9b2363892b1c4
azatkariuly <azat.kariuly@gmail.com>
2023-01-02 02:29:48 +0900

    initial commit

--- cifar10/models/binarized_modules.py
+++ cifar10/models/binarized_modules.py
@@ -38,7 +38,8 @@ def satmm_cuda_temp(A, X, T=64, b=8, signed=True, nbits_psum=8, step_size_psum=N
 
     if step_size_psum is not None:
         # print('doing quantization..')
-        psum, _ = quantizeLSQ_psum(psum, step_size_psum, nbits_psum)
+        
+        # psum, _ = quantizeLSQ_psum(psum, step_size_psum, nbits_psum)
 
         #out = reduce(lambda x,y: (x+y).clip(min, max), psum.transpose(0,3)).squeeze().transpose(0,-1)
         out = OA(torch.sum(psum, axis=3).squeeze().transpose(1,-1), b=b)
