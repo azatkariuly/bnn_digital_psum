a484e4781621cb14aee896e5576ee1e6cbb455ab
azatkariuly <azat.kariuly@gmail.com>
2023-01-08 22:28:39 +0900

    initial commit

--- cifar10/models/resnet_binary.py
+++ cifar10/models/resnet_binary.py
@@ -16,10 +16,10 @@ def Binaryconv3x3(in_planes, out_planes, stride=1, nbits_acc=8, T=64, k=2, s=2):
     #
     # print('optimal_psum:', nbits_acc, 9*in_planes, d[str(9*in_planes)], 2**p_bits)
 
-    if 9*in_planes > 2560:
-        p_bits = 5
-    else:
+    if 9*in_planes < 2560:
         p_bits = 4
+    else:
+        p_bits = 3
 
     print('optimal_psum:', nbits_acc, 9*in_planes, 2**(nbits_acc - p_bits))
 
