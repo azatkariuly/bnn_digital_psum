e3909e005179194e7c907e9dd611d2331d288350
azatkariuly <azat.kariuly@gmail.com>
2023-01-16 20:33:02 +0900

    initial commit

--- cifar10/models/binarized_modules.py
+++ cifar10/models/binarized_modules.py
@@ -51,7 +51,8 @@ def satmm_cuda_temp(A, X, T=64, b=8, signed=True, nbits_psum=8, step_size_psum=N
         # elif shift_value > 4:
         #     shift_value = 4
 
-        torch.save(psum, 'psum.pt')
+        # torch.save(psum, 'psum.pt')
+        print(psum[0].shape)
 
         psum, _ = quantizeLSQ_psum(psum, step_size_psum, nbits_psum)
 
